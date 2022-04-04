79f2643ff7f170f2280f5e577de836815d9abc23
Zijie Zhang <z.zijie@outlook.com>
2022-04-05 01:45:00 +0800

    Sonarlint for 6.ZigZag Conversion

--- 6. ZigZag Conversion.py
+++ 6. ZigZag Conversion.py
@@ -1,20 +1,18 @@
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
+    def convert(self, s: str, num_rows: int) -> str:
         string_length = len(s)
-        group_length = numRows + numRows - 2
+        group_length = num_rows + num_rows - 2
         if group_length == 0:
             return s
-        group_count = string_length // group_length
-        remainder = string_length - group_count * group_length
         new_string = ""
         for idx in range(0, string_length, group_length):
             new_string += s[idx]
-        for start in range(1, numRows - 1):
+        for start in range(1, num_rows - 1):
             for idx in range(start, string_length, group_length):
                 new_string += s[idx]
                 if 0 <= idx - start + group_length - start < len(s):
                     new_string += s[idx - start + group_length - start]
-        for idx in range(numRows - 1, string_length, group_length):
+        for idx in range(num_rows - 1, string_length, group_length):
             new_string += s[idx]
         return new_string
 
